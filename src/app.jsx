ca8f5d3183a4d763fa2f621e62e5e904e9120b7d
John <john@johnsheets.me>
2017-07-27 16:19:36 -0700

    centered body/added bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import './app.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
   render () {
     return (
-      <div>
+      <div className="app">
         <div className="app-title">Music Master</div>
-        <div>
-          <input placeholder="search for an artist...."/>
-          <button>Submit</button>
-        </div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+            />
+          <InputGroup.Addon>
+            <Glyphicon glyph="search"></Glyphicon>
+          </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
         <div className="profile">
           <div>Artist Picture</div>
           <div>Artist Name</div>
