1b7a7023e65d721b5cea06a265f8bebe16d037dd
John <john@johnsheets.me>
2017-07-31 14:05:04 -0700

    layout for artist profile complete

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import './app.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Profile from './profile';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
-    }
+      query: '',
+      artist: null
+    };
   }
 
   search() {
-    console.log('this.state', this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?';
     let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-    var accessToken = '';
+    var accessToken = 'BQAWPPoDisPYVM3uMumcfCUmylh5mSL85xE8R6-fDvbo-m4t13-RZsl9d0gRQMywdRpM9_UnHx_H5FtwF4Kg5pQclRJEheLXUGRdI-SbqQgld5rGNYdnJtgSwSMQL9j7H_7WCL7tFrvzpN01QGpHxwTp9aJaEOn6Wnvo&refresh_token=AQDKv4P02UKakeh2YXMrU-Aa5YcfPg15rkdYeM9nECk0B8hbD7Ag9qlVIDCiLlnQNmdUZ-hjz4c_pJcTEa1nFOS7qssMi-1Ly6FDmqY-DIDr3NHTGft7OQhVjCl2DR7mcYY';
     var myHeaders = new Headers();
     var myOptions = {
       method: 'GET',
@@ -26,7 +27,11 @@ class App extends Component {
     };
     fetch(FETCH_URL, myOptions)
     .then(response => response.json())
-    .then(json => console.log('json', json));
+    .then(json => {
+      const artist = json.artists.items[0];
+      this.setState({artist});
+      console.log('artist', artist);
+    });
   }
 
   render () {
@@ -51,10 +56,9 @@ class App extends Component {
           </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
-        <div className="profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
-        </div>
+        <Profile
+          artist={this.state.artist}
+        />
         <div className="gallery">
           Gallery
         </div>
